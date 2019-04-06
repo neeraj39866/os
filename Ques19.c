ee1c65e343b4dcfaeeef3ca913d5a8ba807724fe
neeraj39866 <43573431+neeraj39866@users.noreply.github.com>
2019-04-07 00:17:03 +0530

    Update Ques19.c

--- Ques19.c
+++ Ques19.c
@@ -1,7 +1,7 @@
 #include<stdio.h> 
 using namespace std; 
-const int a = 5; // Number of processes 
-const int b= 3; // Number of resources 
+ int a = 5; // Number of processes 
+int b= 3; // Number of resources 
 void calneed(int need[a][b], int maxm[a][b], int allot[a][b]) // Function to find the need of each process 
 { 
     for (int i = 0 ; i < a ; i++) 
@@ -12,7 +12,7 @@ bool isSafe(int processes[], int avail[], int maxm[][b],int allot[][b])
 { 
     int need[a][b]; 
     calneed(need, maxm, allot); // Function to calculate need matrix 
-    bool finish[a] = {0};  // Mark all processes as infinish 
+     finish[a] = {0};  // Mark all processes as infinish 
     int safeSeq[a]; // To store safe sequence 
     int work[b]; 
     for (int i = 0; i < b ; i++) //duplicacy in work resource
@@ -22,8 +22,7 @@ bool isSafe(int processes[], int avail[], int maxm[][b],int allot[][b])
     // or system is not in safe state. 
     int count = 0; 
     while (count < a) //find a process which is not finished
-    { 
-        bool found = false; 
+    {  
         for (int c = 0; c < a; c++) 
         {  
             if (finish[c] == 0) //checking process is finished or not
@@ -57,9 +56,9 @@ bool isSafe(int processes[], int avail[], int maxm[][b],int allot[][b])
 int main() 
 { 
     int processes[] = {0, 1, 2, 3, 4}; 
-    int avail[] = {3, 3, 2}; // Available instances of resources 
-    int maxm[][b] = {{7, 5, 3},{3, 2, 2},{9, 0, 2},{2, 2, 2},{4, 3, 3}}; // Maximum R that can be allocated to processes
-    int allot[][b] = {{0, 1, 0}, {2, 0, 0}, {3, 0, 2},{2, 1, 1}, {0, 0, 2}}; // Resources allocated to processes 
+    int avail[] = {5, 3, 2}; // Available instances of resources 
+    int maxm[][b] = {{7, 8, 4},{3, 2, 2},{9, 0, 6},{2, 8, 2},{4, 5, 3}}; // Maximum R that can be allocated to processes
+    int allot[][b] = {{0, 6, 0}, {2, 6, 0}, {3, 7, 2},{2, 6, 1}, {1, 6, 2}}; // Resources allocated to processes 
     isSafe(processes, avail, maxm, allot); // Check system is in safe state or not
     return 0; 
 } 
